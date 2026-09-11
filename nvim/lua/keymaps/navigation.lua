3d3af8dd19f11232f355256a0f607b54fbe13fe4
azumawin <azuolas004@gmail.com>
2026-09-11 15:05:16 +0300

    add navigation shortcuts for moving up and down by 15 lines at a time

---
+++ nvim/lua/keymaps/navigation.lua
@@ -0,0 +1,5 @@
+-- Move cursor down 15 lines
+vim.keymap.set({ "n", "v" }, "<C-j>", "15j", { desc = "Move cursor down 15 lines" })
+
+-- Move cursor up 15 lines
+vim.keymap.set({ "n", "v" }, "<C-k>", "15k", { desc = "Move cursor up 15 lines" })
