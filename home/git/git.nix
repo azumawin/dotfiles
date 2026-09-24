e8947216cd605f08daddc47645ee1567dd08b877
azumawin <azuolas004@gmail.com>
2026-09-24 11:34:46 +0300

    add gitconfig script to set git identity per project

--- home/git/git.nix
+++ home/git/git.nix
@@ -3,8 +3,38 @@
   ...
 }:
 
+let
+  gitconfig = pkgs.writeShellApplication {
+    name = "gitconfig";
+    runtimeInputs = [ pkgs.git ];
+    text = ''
+      case "''${1-}" in
+        azuolasma)
+          name=azuolasma
+          email=azuolas.majauskas@mif.stud.vu.lt
+          ;;
+        azumawin)
+          name=azumawin
+          email=azuolas004@gmail.com
+          ;;
+        *)
+          echo "usage: gitconfig {azuolasma|azumawin}" >&2
+          exit 1
+          ;;
+      esac
+
+      git config user.name "$name"
+      git config user.email "$email"
+      echo "$name <$email>"
+    '';
+  };
+in
+
 {
 
   home.file.".gitconfig".source = ./config/.gitconfig;
-  home.packages = [ pkgs.git ];
+  home.packages = [
+    pkgs.git
+    gitconfig
+  ];
 }
